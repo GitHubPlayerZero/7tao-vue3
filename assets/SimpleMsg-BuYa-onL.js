6a031576e986d6962f276fa13a701e4862695fac
GitHubPlayer0 <65072506+GitHubPlayerZero@users.noreply.github.com>
2025-05-06 17:33:31 +0800

    Updates

---
+++ assets/SimpleMsg-BuYa-onL.js
@@ -0,0 +1,2 @@
+import{a as t,b as o,t as a,o as n}from"./index-QmxKL_5Z.js";const c={class:"container py-3"},r={class:"text-center font-noto-sans-tc fs-18"},m={__name:"SimpleMsg",props:{message:String},setup(s){const e=s;return(p,_)=>(n(),t("div",c,[o("p",r,a(e.message),1)]))}};export{m as _};
+//# sourceMappingURL=SimpleMsg-BuYa-onL.js.map
