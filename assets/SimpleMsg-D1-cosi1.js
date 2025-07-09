52bccda8f9cd8f96defdbdb385246d7e21c0db5f
GitHubPlayer0 <65072506+GitHubPlayerZero@users.noreply.github.com>
2025-07-09 12:24:21 +0800

    Updates

---
+++ assets/SimpleMsg-D1-cosi1.js
@@ -0,0 +1,2 @@
+import{C as t,D as o,M as a,F as n}from"./@vue-CUoatyKe.js";const c={class:"container py-3"},r={class:"text-center font-noto-sans-tc fs-18"},m={__name:"SimpleMsg",props:{message:String},setup(s){const e=s;return(p,_)=>(n(),t("div",c,[o("p",r,a(e.message),1)]))}};export{m as _};
+//# sourceMappingURL=SimpleMsg-D1-cosi1.js.map
