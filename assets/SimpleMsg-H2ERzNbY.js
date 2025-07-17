5629893b1d8c586a1aa7bf47ccaa5a1dede73f01
GitHubPlayer0 <65072506+GitHubPlayerZero@users.noreply.github.com>
2025-07-17 11:36:33 +0800

    Updates

---
+++ assets/SimpleMsg-H2ERzNbY.js
@@ -0,0 +1,2 @@
+import{I as t,K as o,T as a,J as n}from"./@vue-BoO0vas5.js";const c={class:"container py-3"},r={class:"text-center font-noto-sans-tc fs-18"},m={__name:"SimpleMsg",props:{message:String},setup(s){const e=s;return(p,_)=>(n(),t("div",c,[o("p",r,a(e.message),1)]))}};export{m as _};
+//# sourceMappingURL=SimpleMsg-H2ERzNbY.js.map
